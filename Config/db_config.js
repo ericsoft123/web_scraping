6de87faf3e652ab99b1011527f63a2ac104d6755
Eric Kayiranga <ericsoft123@gmail.com>
2021-07-07 07:54:09 +0200

    Remove some config code

--- Config/db_config.js
+++ Config/db_config.js
@@ -1,18 +1,7 @@
 var mysql = require('mysql'); 
 exports.database_config = mysql.createConnection({
   
-    /*host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    database:process.env.DB_DATABASE,
-    password:""*/
-
-    /*host:"sql4.freemysqlhosting.net",
-    port:3306,
-    user:"sql4423462",
-    database:"sql4423462",
-    password:"9iWeUZQN2M",*/
-
-    host:process.env.DB_HOST,
+     host:process.env.DB_HOST,
     port:process.env.DB_PORT,
     user:process.env.DB_USER,
     database:process.env.DB_DATABASE,
